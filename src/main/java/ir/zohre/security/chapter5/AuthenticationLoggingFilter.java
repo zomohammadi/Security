536c730683cd294852904f5abb87447a95bd9214
Zohre <mohammadizohre1371@gmail.com>
2026-02-20 22:48:57 +0330

    use OncePerRequestFilter

--- src/main/java/ir/zohre/security/chapter5/AuthenticationLoggingFilter.java
+++ src/main/java/ir/zohre/security/chapter5/AuthenticationLoggingFilter.java
@@ -2,18 +2,23 @@ package ir.zohre.security.chapter5;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class AuthenticationLoggingFilter implements Filter {
+public class AuthenticationLoggingFilter extends OncePerRequestFilter {
     private final Logger logger = LoggerFactory.getLogger(AuthenticationLoggingFilter.class);
+
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        var httpRequest = (HttpServletRequest) servletRequest;
-        String requestId = httpRequest.getHeader("Request-Id");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestId = request.getHeader("Request-Id");
         logger.info("info(\"Successfully authenticated #B\n" +
                 "request with id: " + requestId);
+
+        filterChain.doFilter(request, response);
     }
 }
